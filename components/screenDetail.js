10ef70684c4bb0b08694d76a98dfc06a2586fa47
SarcXD <sarcxd@gmail.com>
2019-04-29 08:08:30 +0500

    donation requests added

--- components/screenDetail.js
+++ components/screenDetail.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, ScrollView, ActivityIndicator, FlatList, View, Text, TextInput, Button } from 'react-native';
-import {Table, Row, Rows} from 'react-native-table-component';
+import { StyleSheet, ScrollView, ActivityIndicator, View, Text, CheckBox, Button, TouchableOpacity, Alert } from 'react-native';
+import {Table, Row, Cell, TableWrapper} from 'react-native-table-component';
 import firebase from 'firebase'
 
 class DetailScreen extends Component {
@@ -10,8 +10,10 @@ class DetailScreen extends Component {
     this.unsubscribe = null;
     this.state = {
       loading: true,
-      tableHead: ['Item Name','Quantity','Type','Status','Date Added'],
-      tableData: []
+      tableHead: ['Item Name','Quantity','Type','Status','Date Added', 'Donate?'],
+      tableData: [],
+      donateData: [],
+      checked: [],
     };
   }
 
@@ -25,6 +27,7 @@ class DetailScreen extends Component {
 
   onCollectionUpdate = (querySnapshot) => {
     const tableData = [];
+    const chk = [];
     querySnapshot.forEach((doc) => {
       
       var name = doc.data().itemName
@@ -33,14 +36,16 @@ class DetailScreen extends Component {
       var status = doc.data().status
       var date = doc.data().dateAdded
       // var parsed = date.
-      var test=[name, type, quantity, status, date];
-      tableData.push(test)
+      var test=[name, quantity, type, status, date,''];
+      tableData.push(test);
+      chk.push(false);
     });
     // this.state.tableData.push(test)
     this.setState({
       loading: false,
-      tableHead: ['Item Name','Quantity','Type','Status','Date Added'],
-      tableData
+      tableHead: ['Item Name','Quantity','Type','Status','Date Added', 'Donate?'],
+      tableData,
+      checked: chk
    });
   }
 
@@ -53,6 +58,33 @@ class DetailScreen extends Component {
   //     extra: 'fails?'
   //   });
   // }
+
+  sendReq = () => {
+    const data = this.state.donateData;
+    donRef=firebase.firestore().collection('DonationReqs');
+    // var time = String(new Date().getDate())+'/'+String(new Date().getMonth())+'/'+String(new Date().getFullYear());
+    data.forEach((req)=>{
+      // const {name, quantity, type, status, date} = req;
+      donRef.add({
+        itemName: String(req[0]),
+        quantity: parseInt(req[1]),
+        type: String(req[2]),
+        status: String(req[3]),
+        dateAdded: String(req[4]),
+      });
+    }
+    );
+    
+    Alert.alert(
+      'Donation Request',
+      'Donation Request Sent! Please wait while we process it',
+      [
+        {text: 'OK'},
+      ],
+      {cancelable: false},
+    );
+  }
+
   static navigationOptions = {
     title: 'Waste Details',
   };
@@ -65,12 +97,61 @@ class DetailScreen extends Component {
         // null
       );
     }
+    const state = this.state
+    const element = (data, index) => (
+      <TouchableOpacity onPress={() => {
+        var dat = this.state.donateData
+        var finalDat = []
+        if(!this.state.checked[index]){
+          finalDat=dat;
+          finalDat.push(data);
+        }else{
+          dat.forEach(row =>{
+            if(row!=data){
+              finalDat.push(row);
+            }
+          })
+        }
+        const chk=this.state.checked
+        chk[index] = !chk[index]
+        // this.state.donateData.push(dat)
+        this.setState({
+          donateData: finalDat,
+          checked: chk,
+        })
+        // valid
+        // var str="";
+        // dat.forEach(row=>{
+        //   row.forEach(e=>{
+        //     str+=String(e)+' '
+        //   })
+        //   str+='\n'
+        // })
+        // Alert.alert(str)
+        
+      }}>
+        <View style={styles.btn}>
+          {this.state.checked[index] == false? <Text style={styles.btnText}> Add </Text>: <Text style={styles.btnText}> Remove </Text> }
+        </View>
+      </TouchableOpacity>
+    );
+
     return (
-      <View style={{ flex: 1}}>
-        {/* <Text>{this.state.tableData }</Text> */}
-        <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
-        <Row data={this.state.tableHead} style={styles.head} textStyle={styles.text}/>
-        <Rows data={this.state.tableData} textStyle={styles.text}/>
+      <View style={styles.container}>
+        <Table borderStyle={{borderColor: 'transparent'}}>
+          <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
+          {
+            state.tableData.map((rowData, index) => (
+              <TableWrapper key={index} style={styles.row}>
+              {
+                  rowData.map((cellData, cellIndex) => (
+                    <Cell key={cellIndex} data={cellIndex === 5 ? element(rowData, index) : cellData} textStyle={styles.text}/>
+                  ))
+                }
+              </TableWrapper>
+            ))
+          }
+        {/* // <Rows data={this.state.tableData} textStyle={styles.text}/> */}
         </Table>
         {/* <FlatList
           data={this.state.posts}
@@ -86,14 +167,23 @@ class DetailScreen extends Component {
           // disabled={!this.state.name.length}
           onPress={() =>  this.props.navigation.navigate('AddBoard')}
         />
+        <Button
+          title={'Donate'}
+          // disabled={!this.state.donateData.length}
+          onPress={() =>  this.sendReq()}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  head: {height: 40, backgroundColor: 'skyblue'},
-  text: {margin: 6},
+  container: { flex: 1, padding: 1, paddingTop: 30, backgroundColor: '#fff' },
+  head: { height: 40, backgroundColor: '#808B97' },
+  text: { margin: 6 },
+  row: { flexDirection: 'row', backgroundColor: '#FFF1C1' },
+  btn: { width: 58, height: 18, backgroundColor: '#78B7BB',  borderRadius: 2 },
+  btnText: { textAlign: 'center', color: '#fff' }
 });
 
 export default DetailScreen;
