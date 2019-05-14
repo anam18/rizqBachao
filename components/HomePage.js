1401c74d67ea3df4ce168a1655a1d79e5d20240f
anam18 <48842856+anam18@users.noreply.github.com>
2019-05-14 17:43:32 +0500

    UI change

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,87 +1,105 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-// import { Button } from 'react-native-elements';
-// import { reateStackNavigator } from 'react-navigation';
-// import requests from './requests';
-// import SignupDonor from './SignupCenter';
-// import SignupCenter from './SignupCenter';
-// import SignupTransporter from './SignupTransporter';
-// import LoginScreen from './LoginScreen';
-
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground, Image } from 'react-native';
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons"
 
 export default class App extends React.Component {
     render() {
       return (
+
         <View style={styles.container}>
-          
+                <View style={styles.logoContainer1}>
+                  <Image 
+                  style = {{height: 200, width: 200, justifyContent: 'center'}}
+                  source = {require('./../assets/rizq.png')} />
+                </View>
 
-        <View style={styles.logoContainer}>
-          <Text>Welcome to RIZQ</Text>
-          <Text>What are you?</Text>
-          
+                <View style={styles.logoContainer}>
+                  <Text style = {styles.headertext1}>Are you a?</Text>                      
+                </View>
+
+                <View style={styles.formContainer}>
+                
+                  <View>
+                    <TouchableOpacity style={styles.button}
+                      onPress={()=> this.props.navigation.navigate('Log', {next: 'Donor'})}>
+                        <MaterialCommunityIcons name = "food" size={33} color = 'black' /> 
+                        <Text style={styles.btntext}>Donor</Text>                         
+                    </TouchableOpacity>
+                  </View>
+
+                  <View>
+                    <TouchableOpacity style={styles.button}
+                    onPress={()=> this.props.navigation.navigate('Log', {next: 'Center'})}>
+                        <MaterialCommunityIcons name = "office-building" size={33} color = 'black' />
+                        <Text style={styles.btntext}>Rizq Center</Text>
+                    </TouchableOpacity>
+                  </View>
+
+                  <View>
+                    <TouchableOpacity  style={styles.button}
+                    onPress={()=> this.props.navigation.navigate('Log',{next: 'Transporter'})} >
+                        <Ionicons name = "md-car" size={33} color = 'black' />
+                        <Text style={styles.btntext}>Rider</Text>
+                    </TouchableOpacity>   
+                  </View>
+                </View>
+
         </View>
-        <View style={styles.formContainer}>
-          <TouchableOpacity 
-          style={styles.button} 
-          onPress={()=> this.props.navigation.navigate('Log', {next: 'Board'})}
-          >
-            <Text style={styles.btntext}>Donor</Text>
-            
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.button}
-          onPress={()=> this.props.navigation.navigate('Log', {next: 'rizqCent'})}
-          >
-            <Text style={styles.btntext}>Rizq Center</Text>
-          </TouchableOpacity>
-
-          {/* <TouchableOpacity style={styles.button}
-          onPress={()=> this.props.navigation.navigate('SignupTransporter',{next: 'Transporter'})}
-          >
-            <Text style={styles.btntext}>Rider</Text>
-          </TouchableOpacity> */}
-
-
-          
-          
-        </View>
-        </View>
-
-    
         
+              
       );
     }
   }
 
   const styles = StyleSheet.create({
+    logoContainer1: {
+      flex: 1,
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
     container: {
       flex: 1,
       backgroundColor: '#fff',
-      //alignItems: 'center',
       justifyContent: 'center',
     },
     logoContainer: {
       alignItems: 'center',
       justifyContent: 'center',
-
+      marginTop: 20,      
+    },
+    headertext1:{
+      color: "black",
+      fontWeight : 'bold',
+      fontSize: 24,
+      fontFamily: 'monospace'
+    },
+    headertext2:{
+      color: '#ffffff',
+      fontWeight : 'bold',
+      fontSize: 20,
+      fontFamily: 'monospace',
     },
     formContainer: {
-       padding: 20
-  
+      flex: 2, 
+      padding: 20
       },
 
     btntext: {
       color: '#fff',
       fontWeight: 'bold',
+      fontSize: 17,
     },
 
     button: {
       alignSelf: 'stretch',
+      justifyContent: 'center',
       alignItems: 'center',
-      padding:20,
-      backgroundColor: '#59cbbd',
+      padding:10,
+      backgroundColor: "goldenrod",
       marginTop:10,
-      
+      marginLeft: 30,
+      marginRight: 30,
 
     },
 
