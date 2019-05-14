37eeba6e8e2adb9d79da8523723a35f8dff80ceb
anam18 <48842856+anam18@users.noreply.github.com>
2019-05-14 21:28:07 +0500

    Update requests.js

--- components/requests.js
+++ components/requests.js
@@ -1,154 +1,221 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
-// import { List, ListItem  } from 'react-native-elements';
-// import { } from 'react-navigation';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Alert } from 'react-native';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
-//import { finished } from 'stream';
+import { YellowBox } from 'react-native';
+//import { getMaxListeners } from 'cluster';
+
+let comp=(new Date().getDate()) + '/' + (new Date().getMonth()+1) +'/' + new Date().getFullYear()
+//let a='rubabahmed46@gmail.com'
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+export default class SignupDonor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      message:'',
+      email:'',
+      todos:[],
+      items:[],
+    //   comp: (new Date().getDate()) + '/' + (new Date().getMonth()+1) +'/' + new Date().getFullYear()
 
 
+    };
+    this.ref=firebase.firestore().collection('DonationReqs')//.where("Date", "==" ,comp);
+    this.ref2=firebase.firestore().collection('Notifications_Donor');
+    this.unsubscribe=null;
 
-export default class App extends React.Component {
-
-    constructor(props){
-      super(props);
-      
-      this.state={
-        data: [],
-        name: ''
-        
-        
-    
-      }
-      
-    }
-
-    componentWillMount(){
-      // // firebaseApp.database().ref('/Center/' + Address).on('value', (snapshot) => {
-      // //   const userObj = snapshot.val();
-      // //   this.name = userObj.Address;
-
-      // //   //this.avatar = userObj.avatar;
-      // // });
-      // var that=this
-      // let q=firebase.database().ref('Center');
-      
-      
-      
-      // var finished=[];
-      // console.log(finished)
-
-      // q.once('value', snapshot=>{
-       
-      //   snapshot.forEach(function(data){
-      //     Alert.alert("hello");
-         
-      //     let result=data.val();
-          
-      //     //result["key"]=data.key;
-      //     finished.push(result);
-
-
-      //   })
-      // }).then(function(result){
-        
-      //   that.setState({
-      //     data: finished
-          
-          
-      //   })
-        
-      // },
-      // function(reject){
-      //   Alert.alert(reject.message);
-
-      // }
-      // )
-      finished=[]
-      const db = firebase.firestore();
-     // db.settings({timestampsInSnapshots: true});
-      db.collection('DonationReqs').get().then((snapshot)=>{
-        snapshot.docs.forEach(doc=>{
-        finished.push(doc.data().Address)
-        Alert.alert(doc.data().Address)
-        })
-      }, (err)=> Alert.alert(err.message)
-      )
-
-      
-
-      this.setState({
-        data: finished
-      })
-    }
-
-    renderElement(){
-      if (this.state.data.length==0) {
-        return <Text>No new requests</Text>
-      }
-
-      else{
-        this.state.data.forEach(function(data){
-          return <Text>{data}</Text>
-        })
-      }
-   }
-
-    render() {
-      return (
-
-        <View style={styles.container}>
-          { this.renderElement() }
-          {/* <Text>{this.state.name}</Text> */}
-        </View>
-
-        
-        
-        
-         
-        
-    
-        
-      );
-    }
   }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    logoContainer: {
-      alignItems: 'center',
-      justifyContent: 'center',
+  lapsList() {
 
-    },
-    formContainer: {
-       padding: 20
-  
-      },
+    return this.state.items.map((data) => {
+      return (
+        <View><Text>{data}</Text></View>
+      )
+    })
 
-    btntext: {
-      color: '#000000',
-      fontWeight: 'bold',
-    },
+}
+  renderItem = ({item}) => {
+      if (this.state.todos.length==0)
+      {
+        return (
+          <View> No new requests </View>
+        )
+      }
+      else{
+        return (
+          <View style={styles.view2}>
+              <Text style={styles.fortext}>Address: {item.Address}</Text>
+              <Text style={styles.fortext}>Donor: {item.Donor}</Text>
+              {/* <Text style={styles.fortext}>Items: {item.Item.split(',')}</Text> */}
+              {/* let arr=item.Item.split(',')
+              arr.forEach((it)=>{
+              <Text style={styles.fortext}>Item: {it}</Text>
 
-    button: {
-      alignSelf: 'stretch',
-      alignItems: 'center',
-      padding:20,
-      backgroundColor: '#59cbbd',
-      marginTop:10,
+              }) */}
+              <View>{this.lapsList()}</View>
+              {/* <Text style={styles.fortext}>Quantity: {item.Quantity}</Text> */}
+              <View style={styles.container}>
+              <TouchableOpacity style={styles.button} 
+              onPress={()=> this.props.navigation.navigate('workk',{
+                donEmail: item.D_Email,
+                donName: item.Donor,
+                donItem: item.Item,
+                donQuantity: item.Quantity,
+                donDate:item.Date
+             })}
+              >
+              <Text>Accept</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button2}
+              onPress={()=>{
+                this.state.message='Donation Request Rejected'
+                this.state.email=item.D_Email
+                this.ref2.add({
+                  date: item.Date,
+                  Key: item.D_Email,
+                  Message: this.state.message
+                }).then(()=>{
+                  Alert.alert('Request Rejected');
+                  
+                }, (err)=>{
+                  Alert.alert(error.message)
+                })
+                this.props.navigation.navigate('requests')
+              }}
+              >
+              <Text style={styles.btext}>Reject</Text>
+              </TouchableOpacity>
+              </View>
+            <View
+                style={{
+                    marginTop: 10,
+                    borderBottomColor: "goldenrod",
+                    borderBottomWidth: 1,
+                }}
+            />
+              
+          </View>
+           
+        
+    )
+
+      }
       
 
-    },
+  }
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate) 
+  }
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+  onCollectionUpdate = (querySnapshot) => {
+    const todos = [];
+    const items= [];
+    querySnapshot.forEach((doc) => {
+      const { Address, D_Email, Date, Donor, Item, Quantity, Status } = doc.data();
+      
+      todos.push({
+        key: doc.id,
+        doc, // DocumentSnapshot
+        Address,
+        D_Email,
+        Date,
+        Donor,
+        Item,
+        Quantity,
+        Status,
+      });
+      
+      //let tempitems=[]
+      let quan=Quantity.split(',')
+      let arr=Item.split(',')
+      for (i=0; i<arr.length; i++){
+        arr[i]=arr[i]+", "+quan[i]+' kg';
+      }
+      this.state.items=arr
+      
+      
 
-
-    input: {
-        height: 30,
-        backgroundColor: 'rgba(255,255,255,0.6)'
-    }
-  });
+      
+    });
   
+    this.setState({ 
+      todos,
+      // items,
+      loading: false,
+   });
+  }
+  render() {
+    return (
+        <View style={styles.view2}> 
+        <FlatList
+          data={this.state.todos}
+          renderItem={this.renderItem}
+        />
+      
+      
+        </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  view1: {
+    flex: 1,
+    flexDirection: 'row',
+    marginBottom: 3,
+  
+  },
+  view2: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingLeft: 15,
+    paddingRight: 45,
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    marginTop: 20
+  },
+  fortext:{
+      fontSize: 16,
+      alignSelf: 'stretch',
+      height: 30,
+      justifyContent: 'center',
+  },
+  button: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#59cbbd',
+    width: '40%',
+    height: 10,
+    marginTop: 10,
+    justifyContent: 'center'
+  
+  },
+  button2: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#F08080',
+    width: '40%',
+    height: 20,
+    marginTop: 10,
+    justifyContent: 'center'
+  
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  
+  },
+  btext: {
+    textAlign: 'center', 
+  
+  },
+  });
